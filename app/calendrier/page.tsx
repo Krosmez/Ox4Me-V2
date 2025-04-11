dfe6b3664d32c4f8122abaa89c414e762400d2d6
Cyprien De Roef <c.deroef@outlook.fr>
2025-04-11 13:59:24 +0200

    dev: Init components and add exemple Cocktails page

---
+++ app/calendrier/page.tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+    return (
+        <h1>Calendar page</h1>
+    );
+}
---
+++ app/cocktails/page.tsx
@@ -0,0 +1,35 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { cocktails } from '@/lib/cocktails';
+
+export default async function Page() {
+    return (
+        <>
+            <h1>Cocktail page</h1>
+            <div className="space-y-6 mt-6">
+                { cocktails.map((cocktail) => (
+                    <Card
+                        className="w-1/6 h-fit"
+                        key={ cocktail.id }
+                    >
+                        <CardHeader>
+                            <CardTitle>{ cocktail.name }</CardTitle>
+                            <CardDescription>Category: { cocktail.category }</CardDescription>
+                        </CardHeader>
+                        <CardContent>
+                            Ingrédients :
+                            <ul>
+                                { cocktail.ingredients.map((ingredient, index) => (
+                                    <li key={ index }>
+                                        { ingredient.quantity && `${ingredient.quantity} ` }
+                                        { ingredient.name }
+                                    </li>
+                                )) }
+                            </ul>
+                        </CardContent>
+                    </Card>
+                ))
+                }
+            </div>
+        </>
+    );
+}
