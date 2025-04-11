359a721514a3151d8402884f9a95f8adaacd415f
Cyprien De Roef <c.deroef@outlook.fr>
2025-04-11 17:19:15 +0200

    fix: Sidebar positioned and add scroll-bar component

--- app/cocktails/page.tsx
+++ app/cocktails/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import CocktailCard from '@/components/CocktailCard';
+import { ScrollArea } from '@/components/ui/scroll-area';
 import { Switch } from '@/components/ui/switch';
 import { cocktails } from '@/lib/cocktails';
 import { Cocktail } from '@/models';
@@ -19,34 +20,26 @@ export default function Page() {
     }, []);
 
     return (
-        <div className="p-6 w-full">
-            <div className="flex items-center justify-between">
-                <h1 className="text-2xl font-bold">Cocktail page</h1>
-                <div className="flex items-center gap-2">
-                    <label
-                        htmlFor="show-fav"
-                        className="ml-2 text-sm"
-                    >
-                        Afficher seulement les favoris
-                    </label>
-                    <Switch
-                        id="show-fav"
-                        checked={ showOnlyFav }
-                        onCheckedChange={ setShowOnlyFav }
-                    />
+        <ScrollArea className="w-full">
+            <div className="p-6">
+                <div className="flex items-center justify-between">
+                    <h1 className="text-2xl font-bold">Cocktail page</h1>
+                    <div className="flex items-center gap-2">
+                        <label
+                            htmlFor="show-fav"
+                            className="ml-2 text-sm"
+                        >
+                            Afficher seulement les favoris
+                        </label>
+                        <Switch
+                            id="show-fav"
+                            checked={ showOnlyFav }
+                            onCheckedChange={ setShowOnlyFav }
+                        />
+                    </div>
                 </div>
-            </div>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mt-6">
-                { showOnlyFav ? favCocktails.map((cocktail: Cocktail) => (
-                    <CocktailCard
-                        key={ cocktail.id }
-                        cocktail={ cocktail }
-                        favCocktails={ favCocktails }
-                        setFavCocktails={ setFavCocktails }
-                    />
-                ))
-                    :
-                    cocktails.map((cocktail: Cocktail) => (
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-6 mt-6">
+                    { showOnlyFav ? favCocktails.map((cocktail: Cocktail) => (
                         <CocktailCard
                             key={ cocktail.id }
                             cocktail={ cocktail }
@@ -54,8 +47,18 @@ export default function Page() {
                             setFavCocktails={ setFavCocktails }
                         />
                     ))
-                }
+                        :
+                        cocktails.map((cocktail: Cocktail) => (
+                            <CocktailCard
+                                key={ cocktail.id }
+                                cocktail={ cocktail }
+                                favCocktails={ favCocktails }
+                                setFavCocktails={ setFavCocktails }
+                            />
+                        ))
+                    }
+                </div>
             </div>
-        </div>
+        </ScrollArea>
     );
 };
