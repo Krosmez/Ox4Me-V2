f274ddac81342e3817fd26b44484f4a4e96aa6cd
Krosmez <jonathanmaison78@gmail.com>
2025-04-11 15:17:10 +0200

    feat: add simple nav and bottom bar

--- app/cocktails/page.tsx
+++ app/cocktails/page.tsx
@@ -7,24 +7,23 @@ export default async function Page() {
         <>
             <h1 className="text-2xl font-bold">Cocktail page</h1>
             <div className="space-y-6 mt-6">
-                { cocktails.map((cocktail) => (
+                {cocktails.map((cocktail) => (
                     <Card
                         className="w-1/6 h-fit"
-                        key={ cocktail.id }
+                        key={cocktail.id}
                     >
                         <CardHeader>
-                            <CardTitle>{ cocktail.name }</CardTitle>
-                            <CardDescription>Category: { cocktail.category }</CardDescription>
+                            <CardTitle>{cocktail.name}</CardTitle>
+                            <CardDescription>Category: {cocktail.category}</CardDescription>
                         </CardHeader>
                         <CardContent>
                             Ingrédients :
                             <ul>
-                                { cocktail.ingredients.map((ingredient, index) => (
-                                    <li key={ index }>
-                                        { ingredient.quantity && `${ingredient.quantity} ` }
-                                        { ingredient.name }
+                                {cocktail.ingredients.map((ingredient, index) => (
+                                    <li key={index}>
+                                        {ingredient.name}
                                     </li>
-                                )) }
+                                ))}
                             </ul>
                         </CardContent>
                         <CardFooter>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+
+import Link from "next/link";
+import MenuList from "@/components/custom/menu-list";
+import { Geist, Geist_Mono } from "next/font/google";
+import type { Metadata } from "next";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -25,9 +28,15 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body
-                className={ `${geistSans.variable} ${geistMono.variable} antialiased p-6` }
+                className={`${geistSans.variable} ${geistMono.variable} antialiased p-6`}
             >
-                { children }
+                <aside className="hidden md:block w-1/4 h-screen bg-gray-100 p-4">
+                    <MenuList variant="vertical" />
+                </aside>
+                {children}
+                <nav className="fixed bottom-0 left-0 right-0 md:hidden bg-gray-100 p-4 border-t">
+                    <MenuList variant="horizontal" />
+                </nav>
             </body>
         </html>
     );
